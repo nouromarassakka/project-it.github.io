8b37d5144cc4fe515e22f79b368b09907e137e73
nouromarassakka <166214075+nouromarassakka@users.noreply.github.com>
2024-04-30 23:03:56 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function order() {
+    let x;
+  if (confirm("conirm order?")) {
+    x = true;
+  } else {
+    y =false;
+  }
+}
+function check() {
+    let x=document.getElementById('final');
+    let y=document.getElementById('p1');
+    x.style.visibility='visible';
+    const checkedRadio = document.querySelector('input[type="radio"]:checked');
+    const checkedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    let innerHTMLContent = '';
+    if (checkedRadio) {
+      innerHTMLContent += `- ${checkedRadio.id}`;
+      }
+    checkedCheckboxes.forEach((checkbox) => {
+      innerHTMLContent += `- ${checkbox.id}`;
+      });
+      x.querySelector('.f1 p').innerHTML = innerHTMLContent;
+}
+function dark(){
+  let styleLink = document.getElementById('styleLink');
+  let currentStyle = styleLink.getAttribute('href');
+
+  if (currentStyle === 'master.css') {
+    styleLink.setAttribute('href', 'master1.css');
+  } else {
+    styleLink.setAttribute('href', 'master.css');
+  }
+};
